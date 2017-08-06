4ab4dc9175c89b10b295869d45cbcbbe7f844174
Vladimir Golubev <trevorismay@gmail.com>
2017-08-07 00:57:49 +0300

    Misc: Make _highlight depend on project name.

--- Misc.cmake
+++ Misc.cmake
@@ -1,8 +1,8 @@
-add_custom_target(_highlight)
+add_custom_target(_highlight_${CMAKE_PROJECT_NAME})
 
 function(highlight_matching GLOB)
     file(GLOB_RECURSE MATCHED ${GLOB})
-    set_property(TARGET _highlight APPEND PROPERTY SOURCES ${MATCHED})
+    set_property(TARGET _highlight_${CMAKE_PROJECT_NAME} APPEND PROPERTY SOURCES ${MATCHED})
 endfunction()
 
 
