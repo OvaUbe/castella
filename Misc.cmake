b2f69e8539285ba4a9301dd9b43e55df3c703ce3
Vladimir Golubev <trevorismay@gmail.com>
2017-08-08 02:26:56 +0300

    Misc: Make register_definitions(...) project-independent.

--- Misc.cmake
+++ Misc.cmake
@@ -9,16 +9,17 @@ function(highlight_matching GLOB)
     set_property(TARGET _highlight_${PROJECT_NAME} APPEND PROPERTY SOURCES ${MATCHED})
 endfunction()
 
+set(_CMAKEUTILS_DEFINITION_LIST_${PROJECT_NAME} "")
 
 macro(register_definitions)
-    list(APPEND _CMAKEUTILS_DEFINITION_LIST ${ARGN})
+    list(APPEND _CMAKEUTILS_DEFINITION_LIST_${PROJECT_NAME} ${ARGN})
     string_prepend(_CMAKEUTILS_REGISTER_DEFINITIONS_TEMP_LIST "-D" ${ARGN})
     add_definitions(${_CMAKEUTILS_REGISTER_DEFINITIONS_TEMP_LIST})
     unset(_CMAKEUTILS_REGISTER_DEFINITIONS_TEMP_LIST)
 endmacro()
 
 function(dump_definitions)
-    string_append(DUMP "\n" ${_CMAKEUTILS_DEFINITION_LIST})
+    string_append(DUMP "\n" ${_CMAKEUTILS_DEFINITION_LIST_${PROJECT_NAME}})
     string_prepend(DUMP "    " ${DUMP})
     string(REGEX REPLACE "\n$" "" DUMP ${DUMP})
 
