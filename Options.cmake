9f6de1de135f08ed5188cc53038150ed34da15fd
ovaube <trevorismay@gmail.com>
2018-04-29 21:44:54 +0300

    Options: option instead of set.

--- Options.cmake
+++ Options.cmake
@@ -2,6 +2,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/Log.cmake)
 
 
 function(set_option OPTION VALUE DESCRIPTION)
-    set(${OPTION} ${VALUE} PARENT_SCOPE)
-    log(info "${OPTION}: ${VALUE} (${DESCRIPTION})")
+    option(${OPTION} ${DESCRIPTION} ${VALUE})
+    log(info "${OPTION}: ${${OPTION}} (${DESCRIPTION})")
 endfunction()
